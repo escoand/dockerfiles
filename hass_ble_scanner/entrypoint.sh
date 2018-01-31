5ddafc9510ada26e903993b502100856aacd75f6
escoand <escoand@users.noreply.github.com>
2018-01-31 19:05:40 +0100

    more output and use default mqtt interface

--- hass_ble_scanner/entrypoint.sh
+++ hass_ble_scanner/entrypoint.sh
@@ -5,13 +5,17 @@
 SCAN_TIME=${SCAN_TIME:-10}
 MQTT_HOST=${MQTT_HOST:-localhost}
 MQTT_PORT=${MQTT_PORT:-1883}
-TOPIC=${TOPIC:-location/docker}
-WAIT_TIME=${WAIT_TIME:-110}
+TOPIC_PRE=${TOPIC_PRE:-location/}
+TOPIC_POST=${TOPIC_POST:-}
+PAYLOAD=${PAYLOAD:-home}
+WAIT_TIME=${WAIT_TIME:-90}
 
 # main loop
 while true; do
+	hciconfig hci0 reset
 
 	# scan
+	printf "%s scanning for %i seconds\n" "$(date +%Y-%m-%d\ %H:%M:%S)" "$SCAN_TIME"
 	(
 		hcitool lescan &
 		sleep "$SCAN_TIME"
@@ -32,11 +36,12 @@ while true; do
 			-h "$MQTT_HOST" \
 			-p "$MQTT_PORT" \
 			-V mqttv311 \
-			-t "$TOPIC" \
-			-m "{}" \
+			-t "$TOPIC_PRE{}$TOPIC_POST" \
+			-m "$PAYLOAD" \
 			"$@"
 
 	# sleep
+	printf "%s sleeping for %i seconds\n" "$(date +%Y-%m-%d\ %H:%M:%S)" "$WAIT_TIME"
 	sleep "$WAIT_TIME"
 
 done
