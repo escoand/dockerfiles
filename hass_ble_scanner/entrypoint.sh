64dd14044f18ca4619bd30f3104682adcc33fca0
escoand <escoand@users.noreply.github.com>
2018-01-31 15:36:35 +0100

    added hass ble scanner

---
+++ hass_ble_scanner/entrypoint.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+SCAN_TIME=${SCAN_TIME:-10}
+MQTT_HOST=${MQTT_HOST:-localhost}
+MQTT_PORT=${MQTT_PORT:-1883}
+TOPIC=${TOPIC:-location/docker}
+WAIT_TIME=${WAIT_TIME:-110}
+
+# main loop
+while true; do
+
+	# scan
+	(
+		hcitool lescan &
+		sleep "$SCAN_TIME"
+		kill -2 $!
+	) |
+
+	# optimize
+	sed '
+		/^LE Scan ...$/d
+		/ (unknown)$/d
+		s/^/BLE_/
+		s/ .*$//
+	' |
+
+	# publish
+	xargs -i \
+		mosquitto_pub \
+			-h "$MQTT_HOST" \
+			-p "$MQTT_PORT" \
+			-V mqttv311 \
+			-t "$TOPIC" \
+			-m "{}" \
+			"$@"
+
+	# sleep
+	sleep "$WAIT_TIME"
+
+done
