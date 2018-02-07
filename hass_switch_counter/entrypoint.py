e0de237eafafad8dd3806986d146122c0ac3604e
escoand <escoand@users.noreply.github.com>
2018-02-07 20:46:23 +0100

    add logging and make it working

--- hass_switch_counter/entrypoint.py
+++ hass_switch_counter/entrypoint.py
@@ -1,39 +1,52 @@
 #!/usr/bin/env python
 
 import os
-import Rpi.GPIO as GPIO
+import time
+import RPi.GPIO as GPIO
 import paho.mqtt.client as mqtt
 
-channel   = 21               if not os.environ['GPIO']      else os.environ['GPIO']
-host      = 'localhost'      if not os.environ['MQTT_HOST'] else os.environ['MQTT_HOST']
-port      = 1883             if not os.environ['MQTT_PORT'] else os.environ['MQTT_PORT']
-protocol  = mqtt.MQTTv311    if not os.environ['MQTT_OLD']  else mqtt.MQTTv31
-topic     = 'switch/counter' if not os.environ['TOPIC']     else os.environ['TOPIC']
-send_time = 30               if not os.environ['SEND_TIME'] else os.environ['SEND_TIME']
+channel   = os.environ['GPIO']      if os.environ.has_key('GPIO')      else 21
+host      = os.environ['MQTT_HOST'] if os.environ.has_key('MQTT_HOST') else 'localhost'
+port      = os.environ['MQTT_PORT'] if os.environ.has_key('MQTT_PORT') else 1883
+protocol  = mqtt.MQTTv31            if os.environ.has_key('MQTT_V31')  else mqtt.MQTTv311
+topic     = os.environ['TOPIC']     if os.environ.has_key('TOPIC')     else 'switch/counter'
+send_time = os.environ['SEND_TIME'] if os.environ.has_key('SEND_TIME') else 30
+debounce  = 100
 
 count = 0
 lastsend = 0
 
+# logging
+def log(message):
+	print time.strftime('%Y-%m-%d %H:%M:%S'), message
+
 # increment counter
 def on_switch(channel):
+	global count
 	count += 1
 
 # init gpio
+log('init gpio')
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(channel, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.add_event_detect(channel, GPIO.BOTH, callback=on_switch)
+GPIO.add_event_detect(channel, GPIO.BOTH, callback=on_switch, bouncetime=debounce)
 
 # connect to mqtt
+log('connect to mqtt')
 client = mqtt.Client(protocol=protocol)
 client.connect_async(host, port)
 client.loop_start()
 
 # main loop
+log('enter main loop')
 while True:
+	log('sleep %d seconds' % send_time)
+	time.sleep(send_time)
+	log('send count %d' % count)
 	client.publish(topic, count)
 	count = 0
-	time.sleep(send_time)
 
 # clean up
+log('clean up')
 client.loop_stop()
 GPIO.cleanup()
