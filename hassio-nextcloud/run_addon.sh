47a2d0cc955f9efbc67646ff0f6caa4fe28414eb
escoand <escoand@users.noreply.github.com>
2019-07-23 22:17:42 +0200

    make hassio compatible

--- hassio-nextcloud/run_addon.sh
+++ hassio-nextcloud/run_addon.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 set -e
 
-HTML_DIR=/data/www/html
-if [ ! -d "${HTML_DIR}" ]; then
-    mkdir -p "${HTML_DIR}" 
-    chown -R www-data:root "${HTML_DIR}"
-    chmod -R g=u "${HTML_DIR}"
-fi
-SHARE_DIR=/share/nextcloud
-if [ ! -d "${SHARE_DIR}" ]; then
-    mkdir -p "${SHARE_DIR}" 
-    chown -R www-data:root "${SHARE_DIR}"
-    chmod -R g=u "${SHARE_DIR}"
-fi
+for DIR in /data/nextcloud /share/nextcloud; do
+    if [ ! -d "${DIR}" ]; then
+        mkdir -p "${DIR}" 
+        chown -R www-data:root "${DIR}"
+        chmod -R g=u "${DIR}"
+    fi
+done
+
 eval $(jq --raw-output '.env_var | .[] | "export " + .name + "=\"" + .value + "\""' /data/options.json)
 
-#/run-parts.sh
+/entrypoint.sh
