87ecc669d041d779f16041a37a223bea961080c9
escoand <escoand@users.noreply.github.com>
2019-07-25 15:03:01 +0200

    add jq to image

--- hassio-nextcloud/run_addon.sh
+++ hassio-nextcloud/run_addon.sh
@@ -1,13 +1,12 @@
 #!/bin/sh -e
 
 for DIR in /data/nextcloud /share/nextcloud; do
-    if [ ! -d "${DIR}" ]; then
-        mkdir -p "${DIR}" 
-        chown -R www-data:root "${DIR}"
-        chmod -R g=u "${DIR}"
-    fi
+    [ -d "${DIR}" ] && continue
+    mkdir -p "${DIR}" 
+    chown -R www-data:root "${DIR}"
+    chmod -R g=u "${DIR}"
 done
 
 eval $(jq --raw-output '.env_var | .[] | "export " + .name + "=\"" + .value + "\""' /data/options.json)
 
-/entrypoint.sh "$@"
+/entrypoint.sh apache
