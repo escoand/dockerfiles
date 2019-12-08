332d97647d14fbe78e43739fe46804e94eaf3ac3
escoand <escoand@users.noreply.github.com>
2019-12-08 22:40:47 +0100

    better mail sender

--- hassio-nextcloud/run_addon.sh
+++ hassio-nextcloud/run_addon.sh
@@ -12,24 +12,24 @@ done
 export NEXTCLOUD_DATA_DIR=/share/nextcloud
 export NEXTCLOUD_UPDATE=1
 cat << EOF |
-    .mysql.database    MYSQL_DATABASE
-    .mysql.host        MYSQL_HOST
-    .mysql.user        MYSQL_USER
-    .mysql.password    MYSQL_PASSWORD
-    .postgres.database POSTGRES_DB
-    .postgres.host     POSTGRES_HOST
-    .postgres.user     POSTGRES_USER
-    .postgres.password POSTGRES_PASSWORD
-    .redis.host        REDIS_HOST
-    .redis.port        REDIS_HOST_PORT
-    .smtp.host         SMTP_HOST
-    .smtp.secure       SMTP_SECURE
-    .smtp.port         SMTP_PORT
-    .smtp.authtype     SMTP_AUTHTYPE
-    .smtp.name         SMTP_NAME
-    .smtp.password     SMTP_PASSWORD
-    .mail.from_address MAIL_FROM_ADDRESS
-    .mail.domain       MAIL_DOMAIN
+    .mysql.database                             MYSQL_DATABASE
+    .mysql.host                                 MYSQL_HOST
+    .mysql.user                                 MYSQL_USER
+    .mysql.password                             MYSQL_PASSWORD
+    .postgres.database                          POSTGRES_DB
+    .postgres.host                              POSTGRES_HOST
+    .postgres.user                              POSTGRES_USER
+    .postgres.password                          POSTGRES_PASSWORD
+    .redis.host                                 REDIS_HOST
+    .redis.port                                 REDIS_HOST_PORT
+    .smtp.host                                  SMTP_HOST
+    .smtp.secure                                SMTP_SECURE
+    .smtp.port                                  SMTP_PORT
+    .smtp.authtype                              SMTP_AUTHTYPE
+    .smtp.name                                  SMTP_NAME
+    .smtp.password                              SMTP_PASSWORD
+    .mail_sender|select(.!=null)|split("@")[0]  MAIL_FROM_ADDRESS
+    .mail_sender|select(.!=null)|split("@")[1]  MAIL_DOMAIN
 EOF
 while read -r CONF ENVVAR; do
     CMD=$(printf '%s | select(.!=null) | tostring | "export %s=\""+.+"\""' "$CONF" "$ENVVAR")
