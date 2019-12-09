ba36afd9c412ef1d1a4048e3d30950b3d82ea76c
escoand <escoand@users.noreply.github.com>
2019-12-10 00:18:24 +0100

    bring addons on par

---
+++ hassio-nextcloud-ext/run_addon.sh
@@ -0,0 +1,42 @@
+#!/bin/sh -eu
+
+TMP=$(mktemp)
+
+for DIR in /data/nextcloud /share/nextcloud; do
+    [ -d "${DIR}" ] && continue
+    mkdir -p "${DIR}" 
+    chown -R www-data:root "${DIR}"
+    chmod -R g=u "${DIR}"
+done
+
+export NEXTCLOUD_DATA_DIR=/share/nextcloud
+export NEXTCLOUD_UPDATE=1
+cat << EOF |
+    .mysql.database                             MYSQL_DATABASE
+    .mysql.host                                 MYSQL_HOST
+    .mysql.user                                 MYSQL_USER
+    .mysql.password                             MYSQL_PASSWORD
+    .postgres.database                          POSTGRES_DB
+    .postgres.host                              POSTGRES_HOST
+    .postgres.user                              POSTGRES_USER
+    .postgres.password                          POSTGRES_PASSWORD
+    .redis.host                                 REDIS_HOST
+    .redis.port                                 REDIS_HOST_PORT
+    .redis.password                             REDIS_HOST_PASSWORD
+    .smtp.host                                  SMTP_HOST
+    .smtp.secure                                SMTP_SECURE
+    .smtp.port                                  SMTP_PORT
+    .smtp.authtype                              SMTP_AUTHTYPE
+    .smtp.name                                  SMTP_NAME
+    .smtp.password                              SMTP_PASSWORD
+    .mail_sender|select(.!=null)|split("@")[0]  MAIL_FROM_ADDRESS
+    .mail_sender|select(.!=null)|split("@")[1]  MAIL_DOMAIN
+EOF
+while read -r CONF ENVVAR; do
+    CMD=$(printf '%s | select(.!=null) | tostring | "export %s=\""+.+"\""' "$CONF" "$ENVVAR")
+    jq -r "$CMD" /data/options.json >> "$TMP"
+done
+. "$TMP"
+rm -rf "$TMP"
+
+sh /entrypoint.sh "$@"
