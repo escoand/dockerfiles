010a906d457c6d3df3d92e2dd2818581fbaedb15
escoand <escoand@users.noreply.github.com>
2020-09-24 12:42:28 +0200

    Update check.sh

--- smartmon/check.sh
+++ smartmon/check.sh
@@ -4,11 +4,42 @@ MQTT_HOST=${MQTT_HOST:-127.0.0.1}
 MQTT_PORT=${MQTT_PORT:-1883}
 HOSTNAME=$(hostname)
 
+# device info
+ls /dev/sd[a-z] 2>/dev/null |
+while read -r DEV; do
+	NAME=$(basename "$DEV")
+	/usr/sbin/smartctl --info --json "$DEV" |
+	jq -c '{
+		device: {
+			identifiers: .serial_number,
+			manufacturer: .model_family,
+			model: .model_name,
+			name: .serial_number,
+			sw_version: .firmware_version
+		},
+		name: (env.SENSOR + " " + env.DEV),
+		state_topic: ("smartctl/" + env.HOSTNAME + "/" + env.NAME),
+		unit_of_measurement: env.UNIT,
+		value_template: "{{ value_json.ata_smart_error_log.summary.count }}"
+	},
+	{
+		device: { identifiers: .serial_number },
+		name: ("Temperature " + env.DEV),
+		state_topic: ("smartctl/" + env.HOSTNAME + "/" + env.NAME),
+		unit_of_measurement: "°C",
+		value_template: "{{ value_json.temperature.current }}"
+	}' |
+	while read -r LINE; do
+		mosquitto_pub -h "$MQTT_HOST" -p "$MQTT_PORT" -t "homeassistant/sensor/$RANDOM$RANDOM/config" -m "$LINE"
+	done
+done
+
+# data
 while true; do
 	ls /dev/sd[a-z] 2>/dev/null |
 	while read -r DEV; do
 		NAME=$(basename "$DEV")
-		/usr/sbin/smartctl --info --all --json --nocheck standby "$DEV" |
+		/usr/sbin/smartctl --all --json --nocheck standby "$DEV" |
 		mosquitto_pub -h "$MQTT_HOST" -p "$MQTT_PORT" -t "smartctl/$HOSTNAME/$NAME" -s
 	done
 
