006789c487b30aca8ac9bc12868ea8a04ee23936
escoand <escoand@users.noreply.github.com>
2021-06-08 21:23:03 +0200

    Update install.sh

--- homeserver/install.sh
+++ homeserver/install.sh
@@ -4,7 +4,7 @@
 sudo apt-get remove -qy docker docker-engine docker.io runc
 
 # install official docker
-echo "deb [arch=armhf signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" |
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" |
 sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 curl -fsSL https://download.docker.com/linux/debian/gpg |
 sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
