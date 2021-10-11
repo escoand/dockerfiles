6e1bfee8eac9c81e0305c55e2d8c715364fd8011
escoand <escoand@users.noreply.github.com>
2021-10-11 10:04:57 +0200

    docker data on external

--- homeserver/install.sh
+++ homeserver/install.sh
@@ -2,6 +2,7 @@
 set -e
 
 DEVICE=${DEVICE:-/dev/sda1}
+EXTERNAL=${EXTERNAL:-/media/external}
 
 # remove debian dependencies
 sudo apt-get remove -qy docker docker-engine docker.io runc
@@ -15,10 +16,17 @@ sudo apt-get update -qy
 sudo apt-get install -qy containerd.io docker-ce docker-ce-cli libseccomp2/buster-backports
 
 # add mountpoint
-echo "UUID=$(blkid -s UUID -o value $DEVICE) /media/external $(blkid -s TYPE -o value $DEVICE) defaults,noatime 0 2" |
+echo "UUID=$(blkid -s UUID -o value $DEVICE) $EXTERNAL $(blkid -s TYPE -o value $DEVICE) defaults,noatime 0 2" |
 sudo tee -a /etc/fstab > /dev/null
-sudo mkdir /media/external
+sudo mkdir -p "$EXTERNAL"
 sudo mount -a
+
+# docker data on external
+mkdir -p "$EXTERNAL/docker"
+sudo sed -i '$aDOCKER_OPTS="$DOCKER_OPTS --data-root=\"'"$EXTERNAL"'/docker\"' /etc/defaults/docker
+sudo sed -i 's#^ExecStart=.*\.sock$#& --data-root='"$EXTERNAL"'/docker#' /usr/lib/systemd/system/docker.service
+sudo systemctl daemon-reload
+sudo systemctl restart docker
 
 # init repo
 git clone https://github.com/escoand/dockerfiles.git
