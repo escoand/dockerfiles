797b3b2d1f8511758462ed4c47ec1fa4efa46b0f
escoand <escoand@users.noreply.github.com>
2022-09-21 12:56:13 +0200

    add nextcloud

--- homeserver/update.sh
+++ homeserver/update.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
-FILES=""
-BASEDIR=$(dirname "$0")
-cd "$BASEDIR"
+CONTAINERCMD="docker.io/containrrr/watchtower --run-once --cleanup"
+SOCKET=/run/user/$(id -u)/podman/podman.sock
 
-git pull --all &&
-sudo sh -c "
-	docker-compose $FILES pull --no-parallel &&
-	docker-compose $FILES up -d &&
-	docker system prune -af
-"
+git pull --all
+
+# get command
+if command -v podman >/dev/null 2>&1; then
+	podman run --security-opt label=disable -it --rm -v "$SOCKET:/var/run/docker.sock:ro" $CONTAINERCMD
+elif command -v docker >/dev/null 2>&1; then
+	sudo docker -H "unix://$SOCKET" run --rm $CONTAINERCMD
+fi
