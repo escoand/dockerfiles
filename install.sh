962fb75407872c15b14cedd9fcd20b9e424f94df
escoand <escoand@users.noreply.github.com>
2024-06-07 13:21:17 +0200

    Aktualisieren von install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
 set -euo pipefail
+export XDG_RUNTIME_DIR="/run/user/$(id -u)"
 
 # dependencies
-sudo dnf install -y podman
-sudo loginctl enable-linger "$USER"
+sudo dnf install -y firewalld podman
+sudo loginctl enable-linger "$(id -nu)"
 
 # firewall
+systemctl --now enable firewalld
 sudo firewall-cmd --permanent \
   --add-rich-rule "rule family=ipv4 forward-port port=80 protocol=tcp to-port=8080" \
   --add-rich-rule "rule family=ipv6 forward-port port=80 protocol=tcp to-port=8080" \
