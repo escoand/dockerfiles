f19c6bd9b74120fd251015803a8a1ab307db02e8
escoand <escoand@users.noreply.github.com>
2024-06-07 13:22:18 +0200

    Aktualisieren von install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ sudo dnf install -y firewalld podman
 sudo loginctl enable-linger "$(id -nu)"
 
 # firewall
-systemctl --now enable firewalld
+sudo systemctl --now enable firewalld
 sudo firewall-cmd --permanent \
   --add-rich-rule "rule family=ipv4 forward-port port=80 protocol=tcp to-port=8080" \
   --add-rich-rule "rule family=ipv6 forward-port port=80 protocol=tcp to-port=8080" \
