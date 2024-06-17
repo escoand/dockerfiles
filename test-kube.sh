e4ebe0861c148e9df69f6555d78a8434eaa3fe59
escoand <escoand@users.noreply.github.com>
2024-06-17 13:25:12 +0200

    end to end testing

--- test-kube.sh
+++ test-kube.sh
@@ -5,17 +5,43 @@ set -euo pipefail
 trap teardown EXIT TERM INT
 
 KUBEDIR=kube
-TMP=$(mktemp)
+TMP1=$(mktemp)
+TMP2=$(mktemp)
 
 # shellcheck disable=SC2317
 teardown() {
-  [ -s "$TMP" ] && return
-  date > "$TMP"
+  [ -s "$TMP1" ] && return
+  date >"$TMP1"
   echo "# tear down"
   find "$KUBEDIR" secrets.sample.yaml -name '*.yaml' -exec podman kube play --down --force {} \; >/dev/null
   sed -n 's/^[[:blank:]]*claimName:[[:blank:]]*//p' "$KUBEDIR"/*.yaml |
-  xargs podman volume rm -f >/dev/null
-  rm -f "$TMP"
+    xargs podman volume rm -f >/dev/null
+  rm -f "$TMP1" "$TMP2"
+}
+
+getsecret() {
+  podman secret inspect secrets --showsecret --format '{{.SecretData}}' |
+    sed -n "s/^[[:blank:]]*$1:[[:blank:]][[:blank:]]*//p" |
+    base64 -d
+}
+
+endtoend() {
+  DOMAIN=$1
+  PAGE=$2
+  RESULT=$3
+  shift 3
+  if curl -ikLsS --noproxy "*" -o "$TMP2" "$@" \
+    --connect-to "$DOMAIN:80:127.0.0.1:8080" \
+    --connect-to "$DOMAIN:443:127.0.0.1:8443" \
+    "http://$DOMAIN/$PAGE" ||
+    # ignore max redir error
+    [ $? = 47 ]; then
+    grep -Fq "$RESULT" "$TMP2"
+  else
+    echo "#   -> failed"
+    cat "$TMP2"
+    return 1
+  fi
 }
 
 echo "# patch pods for testing"
@@ -37,16 +63,25 @@ echo "# create databases"
 podman pod start mariadb-pod >/dev/null
 podman wait --condition healthy mariadb-pod-mariadb >/dev/null
 sed -n 's/^[[:blank:]]*key:[[:blank:]][[:blank:]]*\(.*\)_db_name.*/\1/p' "$KUBEDIR"/*.yaml |
-xargs -n1 ./db_usr_pwd.sh
+  xargs -n1 ./db_usr_pwd.sh
 
 echo "# start pods"
 podman pod start -a >/dev/null
 
 echo "# wait for healthy state"
 find "$KUBEDIR" -name '*.yaml' |
-sed 's|^.*/\(.*\)\.yaml|\1-pod-\1|' |
-xargs podman wait --condition healthy >/dev/null
-RC=$?
+  sed 's|^.*/\(.*\)\.yaml|\1-pod-\1|' |
+  xargs podman wait --condition healthy >/dev/null
 
-echo "# test ended with RC=$RC"
-exit $RC
+echo "# test Nextcloud end-to-end"
+DOMAIN=$(getsecret nextcloud_domain)
+endtoend "$DOMAIN" status.php '"installed":true'
+
+echo "# test Redir end-to-end"
+DOMAIN=$(getsecret redir_domain)
+TARGET=$(getsecret redir_target)
+endtoend "$DOMAIN" test.php "Location: $TARGET" --max-redirs 1 --no-show-error
+
+echo "# test Wordpress end-to-end"
+DOMAIN=$(getsecret wordpress_domain)
+endtoend "$DOMAIN" wp-admin/install.php "HTTP/1.1 200"
