2bbe0f30450f124dc89bc784ebcda85760cb8542
escoand <escoand@users.noreply.github.com>
2024-09-03 10:52:35 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,16 +8,16 @@ sudo dnf install -y firewalld podman
 sudo loginctl enable-linger "$(id -nu)"
 
 # firewall
-sudo sysctl net.ipv4.ip_unprivileged_port_start=80
-#sudo systemctl --now enable firewalld
-#sudo firewall-cmd --permanent \
-#  --add-rich-rule "rule family=ipv4 forward-port port=80 protocol=tcp to-port=8080" \
-#  --add-rich-rule "rule family=ipv6 forward-port port=80 protocol=tcp to-port=8080" \
-#  --add-rich-rule "rule family=ipv4 forward-port port=443 protocol=tcp to-port=8443" \
-#  --add-rich-rule "rule family=ipv6 forward-port port=443 protocol=tcp to-port=8443" \
-#  --add-rich-rule "rule family=ipv4 forward-port port=443 protocol=udp to-port=8443" \
-#  --add-rich-rule "rule family=ipv6 forward-port port=443 protocol=udp to-port=8443"
-#sudo firewall-cmd --reload
+#sudo sysctl net.ipv4.ip_unprivileged_port_start=80
+sudo systemctl --now enable firewalld
+sudo firewall-cmd --permanent \
+  --add-rich-rule "rule family=ipv4 forward-port port=80 protocol=tcp to-port=8080" \
+  --add-rich-rule "rule family=ipv6 forward-port port=80 protocol=tcp to-port=8080" \
+  --add-rich-rule "rule family=ipv4 forward-port port=443 protocol=tcp to-port=8443" \
+  --add-rich-rule "rule family=ipv6 forward-port port=443 protocol=tcp to-port=8443" \
+  --add-rich-rule "rule family=ipv4 forward-port port=443 protocol=udp to-port=8443" \
+  --add-rich-rule "rule family=ipv6 forward-port port=443 protocol=udp to-port=8443"
+sudo firewall-cmd --reload
 
 # applications
 mkdir -p ~/.config/systemd/user ~/.fetchit
