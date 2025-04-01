5880b260f5e8d9ddb2c04bac47e2ca5cc4cd1931
escoand <escoand@users.noreply.github.com>
2025-04-01 16:05:06 +0200

    add local port forwarding

--- install.sh
+++ install.sh
@@ -18,15 +18,11 @@ sudo firewall-cmd --permanent \
   --add-rich-rule "rule family=ipv6 forward-port port=443 protocol=udp to-port=8443"
 
 # firewall local
-sudo firewall-cmd --permanent --new-policy local-forward
-sudo firewall-cmd --permanent --policy local-forward --add-ingress-zone HOST
-sudo firewall-cmd --permanent --policy local-forward --add-egress-zone ANY
-sudo firewall-cmd --permanent --policy local-forward \
-  --add-rich-rule "rule family=ipv4 forward-port port=80 protocol=tcp to-port=8080 to-addr=127.0.0.1" \
-  --add-rich-rule "rule family=ipv6 forward-port port=80 protocol=tcp to-port=8080 to-addr=::1" \
-  --add-rich-rule "rule family=ipv4 forward-port port=443 protocol=tcp to-port=8443 to-addr=127.0.0.1" \
-  --add-rich-rule "rule family=ipv6 forward-port port=443 protocol=tcp to-port=8443 to-addr=::1" \
-  --add-rich-rule "rule family=ipv4 forward-port port=443 protocol=udp to-port=8443 to-addr=127.0.0.1" \
-  --add-rich-rule "rule family=ipv6 forward-port port=443 protocol=udp to-port=8443 to-addr=::1"
-  
+sudo firewall-cmd --permanent --direct --add-rule ipv4 nat OUTPUT 0 -o lo -p tcp --dport 80 -j REDIRECT --to-port=8080
+sudo firewall-cmd --permanent --direct --add-rule ipv6 nat OUTPUT 0 -o lo -p tcp --dport 80 -j REDIRECT --to-port=8080
+sudo firewall-cmd --permanent --direct --add-rule ipv4 nat OUTPUT 0 -o lo -p tcp --dport 443 -j REDIRECT --to-port=8443
+sudo firewall-cmd --permanent --direct --add-rule ipv6 nat OUTPUT 0 -o lo -p tcp --dport 443 -j REDIRECT --to-port=8443
+sudo firewall-cmd --permanent --direct --add-rule ipv4 nat OUTPUT 0 -o lo -p udp --dport 443 -j REDIRECT --to-port=8443
+sudo firewall-cmd --permanent --direct --add-rule ipv6 nat OUTPUT 0 -o lo -p udp --dport 443 -j REDIRECT --to-port=8443
+
 sudo firewall-cmd --reload
