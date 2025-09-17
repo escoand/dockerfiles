4ff2aefe32117d716b3a01d57706b51dc5c2a6d8
escoand <escoand@users.noreply.github.com>
2025-09-17 08:58:17 +0200

    merge install scripts

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
-#!/bin/sh
+#!/bin/bash
+# shellcheck disable=SC2155
 
 set -euo pipefail
 export XDG_RUNTIME_DIR="/run/user/$(id -u)"
@@ -9,3 +10,12 @@ sudo loginctl enable-linger "$(id -nu)"
 
 # allow privileged ports
 sudo sysctl net.ipv4.ip_unprivileged_port_start=80
+
+# fail2ban handler script
+cp fail2ban-fw/*.sh /usr/local/bin/
+chmod 0755 /usr/local/bin/fail2ban-fw.sh
+
+# fail2ban socket and service
+cp -- fail2ban-fw/*.service fail2ban-fw/*.socket /etc/systemd/system/
+sudo systemctl daemon-reload
+sudo systemctl enable --now fail2ban-fw.socket
