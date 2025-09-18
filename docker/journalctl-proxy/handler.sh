a348fd87e68af3e0baadaf3b6bcba6531ad169cd
escoand <escoand@users.noreply.github.com>
2025-09-18 12:13:29 +0200

    add systemd services

--- docker/journalctl-proxy/handler.sh
+++ docker/journalctl-proxy/handler.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export JOURNAL_ID=__journal___
+
 shopt -s lastpipe
 
 urldecode() {
@@ -18,6 +20,12 @@ result() {
     echo "  > result $code: $*" >&2
     printf 'HTTP/1.1 %i %s\r\nApi-Version: 1.41\r\n' "$code" "$*"
     [ "$headers" != FALSE ] && printf 'Connection: close\r\n\r\n'
+}
+
+findService() {
+    systemctl --user show --type=service --property=Id "$1*" |
+        head -n1 |
+        cut -d= -f2-
 }
 
 info() {
@@ -41,69 +49,60 @@ END
 }
 
 list() {
-    start=$(awk '/^btime/ {print $2}' /proc/stat)
-    cat <<END
-[
-  {
-    "Id": "$CONTAINER_ID",
-    "Names": ["/$CONTAINER_NAME"],
-    "Image": "docker.io/library/alpine:latest",
-    "ImageID": "sha256:9234e8fb04c47cfe0f49931e4ac7eb76fa904e33b7f8576aec0501c085f02516",
-    "Command": "/bin/sh",
-    "Created": $start,
-    "Ports": [],
-    "Labels": {},
-    "State": "running",
-    "NetworkSettings": { "Networks": null },
-    "Mounts": [],
-    "Name": "",
-    "Config": null,
-    "NetworkingConfig": null,
-    "Platform": null,
-    "DefaultReadOnlyNonRecursive": false
-  }
-]
-END
+    systemctl --user list-units --all --output=json --type=service |
+        jq '. | to_entries | map(.value += {
+            Created: null,
+            Id: .value.unit,
+            Names: ["/" + .value.unit],
+            State: .value.sub
+        } | .value) + [{
+            Created: null,
+            Id: env.JOURNAL_ID,
+            Names: ["/" + env.JOURNAL_ID],
+            State: "running"
+        }]'
 }
 
 container() {
-    start=$(awk '/^btime/ {print "@" $2}' /proc/stat | xargs date -u -Ins -d)
-    cat <<END
-{
-  "Id": "$CONTAINER_ID",
-  "Created": "$start",
-  "Path": "/bin/sh",
-  "Args": ["/bin/sh"],
-  "State": {
-    "Status": "running",
-    "Running": true,
-    "Paused": false,
-    "Restarting": false,
-    "OOMKilled": false,
-    "Dead": false,
-    "Pid": 4470,
-    "ExitCode": 0,
-    "Error": "",
-    "StartedAt": "$start",
-    "FinishedAt": "0001-01-01T00:00:00Z"
-  },
-  "Image": "sha256:9234e8fb04c47cfe0f49931e4ac7eb76fa904e33b7f8576aec0501c085f02516",
-  "Name": "/$CONTAINER_NAME",
-  "RestartCount": 0,
-  "Driver": "overlay",
-  "Platform": "linux",
-  "MountLabel": "",
-  "ProcessLabel": "",
-  "AppArmorProfile": "",
-  "ExecIDs": [],
-  "HostConfig": {},
-  "GraphDriver": {},
-  "SizeRootFs": 0,
-  "Mounts": [],
-  "Config": {},
-  "NetworkSettings": {}
-}
-END
+    #systemctl --user show "$A*" |
+    #jq --slurp --raw-input 'split("\n") | map(select(.!="") | split("=") | {"key": .[0], "value": (.[1:] | join("="))}) | from_entries' |
+    # shellcheck disable=SC2016
+    systemctl --user --output=json status "$1" |
+        sed -n '/^{/,${p}' |
+        jq '{
+            Id: .USER_UNIT,
+            Created: .__REALTIME_TIMESTAMP,
+            Path: ._EXE,
+            Args: ._CMDLINE,
+            State: {
+                Status: "running",
+                Running: true,
+                Paused: false,
+                Restarting: false,
+                OOMKilled: false,
+                Dead: false,
+                Pid: ._PID,
+                ExitCode: 0,
+                Error: "",
+                StartedAt: .__REALTIME_TIMESTAMP,
+                FinishedAt: "0001-01-01T00:00:00Z"
+            },
+            Image: "sha256:9234e8fb04c47cfe0f49931e4ac7eb76fa904e33b7f8576aec0501c085f02516",
+            Name: ("/" + .USER_UNIT),
+            RestartCount: 0,
+            Driver: "overlay",
+            Platform: "linux",
+            MountLabel: "",
+            ProcessLabel: "",
+            AppArmorProfile: "",
+            ExecIDs: [],
+            HostConfig: {},
+            GraphDriver: {},
+            SizeRootFs: 0,
+            Mounts: [],
+            Config: {},
+            NetworkSettings: {}
+        }'
 }
 
 stats() {
@@ -134,17 +133,15 @@ stats() {
 END
 }
 
-logs() {
-    journalctl "$@"
-}
-
 # request
 typeset method uri
 while read -r key value; do
     if [ -z "$method" ]; then
         method=$key
         uri=$(urldecode "${value% *}")
-        echo "$method $uri" >&2
+        query=${uri#*\?}
+        uri=${uri%%\?*}
+        echo "$method $uri?$query" >&2
     # end of headers
     elif [ -z "$key" ] || [ "$key" = $'\r' ]; then
         break
@@ -160,18 +157,20 @@ elif [[ "$uri" = /*/info ]]; then
 elif [[ "$uri" = /*/events ]]; then
     result 200 OK
     sleep infinity
-elif [[ "$uri" = /*/containers/json* ]]; then
+elif [[ "$uri" = /*/containers/json ]]; then
     result 200 OK
     list
-elif [[ "$uri" = /*/containers/$CONTAINER_ID/json* ]]; then
+elif [[ "$uri" = /*/containers/*/json ]]; then
+    container=$(echo "$uri" | cut -d/ -f4)
+    service=$(findService "$container")
     result 200 OK
-    container
-elif [[ "$uri" = /*/containers/$CONTAINER_ID/stats* ]]; then
+    container "$service"
+elif [[ "$uri" = /*/containers/*/stats ]]; then
     result 200 OK
     stats
-elif [[ "$uri" = /*/containers/$CONTAINER_ID/logs* ]]; then
-    typeset relative args=()
-    echo "${uri#*\?}" | tr '&' '\n' | while IFS='=' read -r key value; do
+elif [[ "$uri" = /*/containers/*/logs ]]; then
+    typeset relative args=(--quiet)
+    echo "$query" | tr '&' '\n' | while IFS='=' read -r key value; do
         if [ "$key" = follow ] && [ -n "$value" ]; then
             args[${#args[@]}]=--follow
         elif [ "$key" = since ]; then
@@ -194,18 +193,26 @@ elif [[ "$uri" = /*/containers/$CONTAINER_ID/logs* ]]; then
             fi
         fi
     done
+    container=$(echo "$uri" | cut -d/ -f4)
+    if [ "$container" != "$JOURNAL_ID" ]; then
+        service=$(findService "$container")
+        args[${#args[@]}]="USER_UNIT=$service"
+    fi
     result -h 200 OK
-    printf "Transfer-Encoding: chunked\r\n\r\n"
-    logs "${args[@]}" |
-    awk '{
-        len  = length($0) + 2
-        b1 = int(len / (2^24)) % 256
-        b2 = int(len / (2^16)) % 256
-        b3 = int(len / (2^8))  % 256
-        b4 = len               % 256
-        printf("%x\r\n%c%c%c%c%c%c%c%c%s\r\n\r\n", len + 8, 2,0,0,0, b1,b2,b3,b4, $0)
-    }
-    '
+    journalctl "${args[@]}" |
+        gawk '
+            BEGIN { printf "Transfer-Encoding: chunked\r\n\r\n" }
+            {
+                len  = length($0) + 2
+                b1 = int(len / (2^24)) % 256
+                b2 = int(len / (2^16)) % 256
+                b3 = int(len / (2^8))  % 256
+                b4 = len               % 256
+                printf("%x\r\n%c%c%c%c%c%c%c%c%s\r\n\r\n", len + 8, 2,0,0,0, b1,b2,b3,b4, $0)
+                system("")
+            }
+            END { printf("0\r\n\r\n") }
+        '
 else
     result 404 Not Found
 fi
