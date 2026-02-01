59336a6c78a261708c4deef07b184b0099079604
escoand <escoand@users.noreply.github.com>
2026-02-01 09:18:02 +0100

    Update entrypoint.sh

--- docker/monitor/entrypoint.sh
+++ docker/monitor/entrypoint.sh
@@ -25,9 +25,9 @@ send_mail() {
 
 send_matrix() {
     UUID=$(uuidgen)
-    jq -R '{msgtype:"m.text",body:.}' |
+    jq -cR '{msgtype:"m.text",body:.}' |
     tee /dev/stderr
-    curl -sS -XPUT \
+    curl -XPUT \
         -H "Authorization: Bearer ${MATRIX_ACCESS_TOKEN}" \
         "http://synapse:8008/_matrix/client/v3/rooms/${MATRIX_ROOM_ID}/send/m.room.message/$UUID"
 }
