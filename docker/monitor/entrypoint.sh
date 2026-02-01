5a8d054b1795c7ab5aa8e00d561a83a15272652d
escoand <escoand@users.noreply.github.com>
2026-02-01 09:01:37 +0100

    Update entrypoint.sh

--- docker/monitor/entrypoint.sh
+++ docker/monitor/entrypoint.sh
@@ -5,6 +5,32 @@ set -e
 
 WAIT=60
 
+send_mail() {
+    {
+        echo "Subject: Docker events"
+        echo
+        cat
+    } |
+        msmtp \
+            --tls=on \
+            --tls-starttls=off \
+            --auth=plain \
+            --host="${MAIL_HOST?}" \
+            --port="${MAIL_PORT:-465}" \
+            --user="${MAIL_USER?}" \
+            --passwordeval='cat /run/secrets/MAIL_PASSWORD' \
+            --from="${MAIL_FROM:-MAIL_TO}" \
+            "${MAIL_TO?}"
+}
+
+send_matrix() {
+    UUID=$(uuidgen)
+    jq '{msgtype:"m.text",body:.}' |
+    curl -v -XPUT \
+        -H "Authorization: Bearer ${MATRIX_ACCESS_TOKEN}" \
+        "http://synapse:8008/_matrix/client/v3/rooms/${MATRIX_EVENT_ROOM}/send/m.room.message/$UUID"
+}
+
 # main loop
 curl -fNsS --unix-socket /var/run/docker.sock http://localhost/events |
     jq -r --unbuffered '
@@ -20,25 +46,12 @@ curl -fNsS --unix-socket /var/run/docker.sock http://localhost/events |
         END=$(($(date +%s) + WAIT))
         REMAINING=$WAIT
         {
-            echo "Subject: Docker events"
-            echo
-            echo "$LINE" | tee /dev/stderr
             while read -r -t $REMAINING LINE; do
                 echo "$LINE" | tee /dev/stderr
                 REMAINING=$((END - $(date +%s)))
                 [ $REMAINING -lt 0 ] && break
             done
         } |
+        send_matrix
 
-            # send mail
-            msmtp \
-                --tls=on \
-                --tls-starttls=off \
-                --auth=plain \
-                --host="${MAIL_HOST?}" \
-                --port="${MAIL_PORT:-465}" \
-                --user="${MAIL_USER?}" \
-                --passwordeval='cat /run/secrets/MAIL_PASSWORD' \
-                --from="${MAIL_FROM:-MAIL_TO}" \
-                "${MAIL_TO?}"
     done
