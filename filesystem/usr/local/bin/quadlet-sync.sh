3c706e2a60d38c13bc5053fd60ae4250be910c7e
escoand <escoand@users.noreply.github.com>
2026-05-22 09:53:30 +0200

    correct placeholder

--- filesystem/usr/local/bin/quadlet-sync.sh
+++ filesystem/usr/local/bin/quadlet-sync.sh
@@ -2,25 +2,18 @@
 
 set -e
 
-changed=0
+rm -f "$MARKER"
 
 if [ ! -d "$LOCAL/.git" ]; then
     git clone --branch "$REPOBRANCH" "$REPO" "$LOCAL"
     mkdir -p ~/.config/containers
     ln -sfn "$LOCAL/$REPODIR" ~/.config/containers/systemd
-    changed=1
+    touch "$MARKER"
 else
     before_rev=$(git -C "$LOCAL" rev-parse HEAD)
     git -C "$LOCAL" pull --ff-only
     after_rev=$(git -C "$LOCAL" rev-parse HEAD)
 
-    if [ "$before_rev" != "$after_rev" ]; then
-        changed=1
-    fi
-fi
-
-if [ "$changed" -eq 1 ]; then
+    [ "$before_rev" != "$after_rev" ] &&
     touch "$MARKER"
-else
-    rm -f "$MARKER"
 fi
