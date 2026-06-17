b1e7314af118c01e35e6f3a0727ddeed288826ab
escoand <escoand@users.noreply.github.com>
2026-06-17 20:46:35 +0200

    merge reload and sync

--- filesystem/usr/local/bin/quadlet-sync.sh
+++ filesystem/usr/local/bin/quadlet-sync.sh
@@ -1,19 +1,68 @@
 #!/bin/sh
 
-set -e
+set -eux
 
-rm -f "$MARKER"
+restart_units=$(mktemp)
+stop_units=$(mktemp)
 
+# shellcheck disable=SC2064
+trap "rm -f '$restart_units' '$stop_units'" EXIT INT TERM HUP
+
+map_path_to_unit() {
+	echo "$1" | sed -n '
+		s#.*/##g
+		s#@\.#*.#
+		s#\.container$##p
+		s#\.kube$##p
+		s#\.pod$#-pod#p
+		s#\.network$#-network#p
+		s#\.volume$#-volume#p
+		s#\.build$#-build#p
+		s#\.image$#-image#p
+		s#\.artifact$#-artifact#p
+	'
+}
+
+# update repo
 if [ ! -d "$LOCAL/.git" ]; then
-    git clone --branch "$REPOBRANCH" "$REPO" "$LOCAL"
-    mkdir -p ~/.config/containers
-    ln -sfn "$LOCAL/$REPODIR" ~/.config/containers/systemd
-    touch "$MARKER"
+	git clone --branch "$REPOBRANCH" "$REPO" "$LOCAL"
+	before_rev=$(git -C "$LOCAL" hash-object -t tree /dev/null)
 else
-    before_rev=$(git -C "$LOCAL" rev-parse HEAD)
-    git -C "$LOCAL" pull --ff-only
-    after_rev=$(git -C "$LOCAL" rev-parse HEAD)
-
-    [ "$before_rev" != "$after_rev" ] &&
-    touch "$MARKER"
+	before_rev=$(git -C "$LOCAL" rev-parse HEAD)
+	git -C "$LOCAL" pull --ff-only
 fi
+after_rev=$(git -C "$LOCAL" rev-parse HEAD)
+
+# link to systemd
+mkdir -p ~/.config/containers
+ln -fsn "$LOCAL/$REPODIR" ~/.config/containers/systemd
+/usr/bin/systemctl --user daemon-reload
+
+# find changed quadlets
+git -C "$LOCAL" diff --name-status --find-renames "$before_rev" "$after_rev" -- "$REPODIR" |
+	while IFS="$(printf '\t')" read -r status old_path new_path; do
+		[ -n "$status" ] || continue
+		old_unit=$(map_path_to_unit "$old_path")
+		new_unit=$(map_path_to_unit "$new_path")
+
+		case "$status" in
+		D)
+			echo "$old_unit" >"$stop_units"
+			;;
+		R*)
+			echo "$old_unit" >"$stop_units"
+			echo "$new_unit" >"$restart_units"
+			;;
+		*)
+			echo "$old_unit" >"$restart_units"
+			;;
+		esac
+	done
+
+# stop units
+cat "$stop_units" |
+	xargs /usr/bin/systemctl --user stop
+
+# restart units
+cat "$restart_units" |
+	xargs /usr/bin/systemctl --user try-restart
